221cc1774acfe6c1c58dfbe746ec65b2db3a35af
Wendler Eis <wendler_eis@hotmail.com>
2020-04-01 00:57:04 -0300

    fix: adjust prettier to codacy

--- src/database/migrations/20200328201808-create-users.js
+++ src/database/migrations/20200328201808-create-users.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', {
+    return queryInterface.createTable("users", {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -29,6 +29,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('users');
+    return queryInterface.dropTable("users");
   },
 };
