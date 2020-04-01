a77d0cb2994bd464ce49fd53e02bfbfaf516f739
Wendler Eis <wendler_eis@hotmail.com>
2020-04-01 00:11:08 -0300

    feat: inject model in associate method

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,14 @@
 import Sequelize from "sequelize";
 import databaseConfig from "../config/database";
 
-import User from "../app/models/User";
 import Event from "../app/models/Event";
+import EventType from "../app/models/EventType";
+import Team from "../app/models/Team";
+import TeamRole from "../app/models/TeamRole";
+import User from "../app/models/User";
 import UserTeam from "../app/models/UserTeam";
 
-const models = [User, Event, UserTeam];
+const models = [Event, EventType, Team, TeamRole, User, UserTeam];
 
 class Database {
   constructor() {
@@ -15,6 +18,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
     models.map((model) => model.init(this.connection));
+    models.map(
+      (model) => model.associate && model.associate(this.connection.models)
+    );
   }
 }
 
